e58d526792115746191d962f70de9328b6dc7821
Deyan Iolov <diolov@gmail.com>
2022-04-07 21:57:49 -0400

    footer links

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,18 @@
 <!-- Footer-->
-<footer class="footer bg-black small text-center text-white-50">
-  <div class="container px-4 px-lg-5">Copyright &copy; Thonic 2022</div>
+<footer class="footer bg-black text-white-50">
+  <div class="container px-4 px-lg-5">
+    <div class="row">
+      <div class="col-12 col-md-6 text-center text-md-start">
+        Connect with us:
+        <div class="social-links">
+          <a href="https://twitter.com/Thonic_crypto" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.reddit.com/r/thonic/" target="_blank"><i class="fab fa-reddit"></i></a>
+          <a href="https://t.me/+ojQiMqZaO_s5ZTM5" target="_blank"><i class="fab fa-telegram"></i></a>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 text-center text-md-end pt-3">
+        <span class="small">Copyright &copy; Thonic <?= date('Y') ?></span>
+      </div>
+    </div>
+  </div>
 </footer>
