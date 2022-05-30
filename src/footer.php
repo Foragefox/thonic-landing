57e5ca5388d9326a917da81188a9d7fc8129a08e
Demetri <dmetri333@gmail.com>
2022-05-30 15:17:53 -0400

    Fixed twitter account

--- src/footer.php
+++ src/footer.php
@@ -5,7 +5,7 @@
       <div class="col-12 col-md-6 text-center text-md-start">
         Connect with us:
         <div class="social-links">
-          <a href="https://twitter.com/Thonic_crypto" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://twitter.com/thonicf" target="_blank"><i class="fab fa-twitter"></i></a>
           <a href="https://www.reddit.com/r/thonic/" target="_blank"><i class="fab fa-reddit"></i></a>
           <a href="https://t.me/+ojQiMqZaO_s5ZTM5" target="_blank"><i class="fab fa-telegram"></i></a>
         </div>
