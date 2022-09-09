5f3ab46da690f85171899bed2942dcebbac78009
Deyan Iolov <diolov@gmail.com>
2022-09-09 14:09:22 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cp -r public/ dist/ \
   && php src/index.php > dist/index.html \
-  && php src/whitelist.php > dist/whitelist.html
+  && php src/whitelist.php > dist/whitelist.html \
   && php src/exchange.php > dist/exchange.html
