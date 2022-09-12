de206801bb8a8d4412b0886444b0dd123b161739
Deyan Iolov <diolov@gmail.com>
2022-09-12 21:33:11 +0100

    update telegram link

--- src/footer.php
+++ src/footer.php
@@ -7,7 +7,7 @@
         <div class="social-links">
           <a href="https://twitter.com/thonicf" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a>
           <a href="https://www.reddit.com/r/thonic/" target="_blank" rel="noopener"><i class="fab fa-reddit"></i></a>
-          <a href="https://t.me/+ojQiMqZaO_s5ZTM5" target="_blank" rel="noopener"><i class="fab fa-telegram"></i></a>
+          <a href="https://t.me/thonicf" target="_blank" rel="noopener"><i class="fab fa-telegram"></i></a>
         </div>
       </div>
       <div class="col-12 col-md-6 text-center text-md-end pt-3">
