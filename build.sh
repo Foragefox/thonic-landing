a339851a1661830e6ae96f381f790ec634cb66ee
Deyan Iolov <diolov@gmail.com>
2022-10-27 10:47:19 +0300

    add proposal application page

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cp -r public/ dist/ \
   && php src/index.php > dist/index.html \
   && php src/404.php > dist/404.html \
+  && php src/proposal-application.php > dist/proposal-application.html \
   && php src/exchange.php > dist/exchange.html
---
+++ src/proposal-application.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "parts/docHead.php" ?>
+
+<body>
+  <?php if (!empty($_SERVER["GOOGLE_TAG_MANAGER"])) { ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $_SERVER["GOOGLE_TAG_MANAGER"] ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+  <?php } ?>
+
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="/img/logo-green.svg" alt="Thonic Logo" class="logo fixed" />
+        <img src="/img/logo-black.svg" alt="Thonic Logo" class="logo black" />
+        THONIC
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-auto">
+          <?php include "parts/navigation.php" ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Whitelist-->
+  <section class="whitelist-section section-band mt-5">
+    <div class="container px-4 px-lg-5">
+      <div class="row">
+        <div class="col-md-11 col-lg-9 col-xl-8 mx-auto text-center">
+          <h1 class="text-navy h3">Proposal Application</h1>
+          <h5>If you would like to work with the Thonic team, please submit your propsal here.</h5>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php include "parts/footer.php" ?>
+</body>
+
+</html>
