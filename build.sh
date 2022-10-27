40f1268891ed1225f7ca3454ca970ac39550de2b
Deyan Iolov <diolov@gmail.com>
2022-10-27 10:44:16 +0300

    Remove whitelist page

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 cp -r public/ dist/ \
   && php src/index.php > dist/index.html \
   && php src/404.php > dist/404.html \
-  && php src/whitelist.php > dist/whitelist.html \
   && php src/exchange.php > dist/exchange.html
