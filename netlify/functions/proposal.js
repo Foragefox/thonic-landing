1e002b329aa7a09af663ef2fcf6e35a91aee979f
Deyan Iolov <diolov@gmail.com>
2022-10-30 10:08:47 +0000

    add validation, clean up code

--- netlify/functions/proposal.js
+++ netlify/functions/proposal.js
@@ -1,32 +1,41 @@
 const { parseMultipartForm } = require('./helpers/parseMultipartForm');
 const { uploadFile, getPublicUrl } = require('./helpers/googleDrive');
+const { HttpStatus } = require('./helpers/HttpStatus');
+
+function respond(code, body) {
+  return {
+    statusCode: code,
+    body: JSON.stringify(body),
+  };
+}
 
 module.exports.handler = async (event) => {
   try {
     if (!event.body || !event.isBase64Encoded) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: "no data",
-        }),
-      };
+      return respond(HttpStatus.BAD_REQUEST, { message: "no data" });
     }
 
     // Attempt to process file and fields sent up in the request using busboy
     const { files, fields } = await parseMultipartForm(event);
 
-    // Upload to Google Drive
-    const file = files[0];
-
-    if (!file) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: "no file uploaded",
-        }),
-      };
+    if (fields.confirm) {
+      console.log("honey pot");
+      return respond(HttpStatus.OK, { message: "Application accepted" });
     }
 
+    const { firstName, lastName, email, walletAddress, source } = fields;
+    if (![firstName, lastName, email, walletAddress].every(e => !!e)) {
+      errorMessage = "Missing required field";
+      console.log(errorMessage);
+      return respond(HttpStatus.BAD_REQUEST, { message: errorMessage });
+    }
+
+    const file = files[0];
+    if (!file) {
+      return respond(HttpStatus.BAD_REQUEST, { message: "No file uploaded" });
+    }
+
+    // Upload to Google Drive
     const uploadedFile = await uploadFile(file.filename, {
       fileContent: file.file,
       mimeType: file.contentType,
@@ -34,30 +43,18 @@ module.exports.handler = async (event) => {
       parents: [process.env.GOOGLE_DRIVE_PROPOSALS_FOLDER_ID],
     });
 
-    if (uploadedFile.status !== 200) {
-      return {
-        statusCode: uploadedFile.status,
-        body: JSON.stringify({
-          statusText: uploadedFile.statusText,
-        }),
-      };
+    if (uploadedFile.status !== HttpStatus.OK) {
+      return respond(uploadedFile.status, {
+        statusText: uploadedFile.statusText,
+      });
     }
 
     // return the file ID and URL for viewing on the client
     const fileUrl = await getPublicUrl(uploadedFile.data.id);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        fileId: uploadedFile.id,
-        fileUrl,
-      }),
-    };
+    return respond(HttpStatus.OK, { message: "Proposal saved" });
   } catch (error) {
     console.log({ error });
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error }),
-    };
+    return respond(HttpStatus.INTERNAL_SERVER_ERROR, { error });
   }
 }
