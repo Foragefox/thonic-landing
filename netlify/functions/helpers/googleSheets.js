fc5b64f7c66ed3146b7193d003134a6c49c1c145
Deyan Iolov <diolov@gmail.com>
2022-10-30 10:07:15 +0000

    add google sheets helper

---
+++ netlify/functions/helpers/googleSheets.js
@@ -0,0 +1,36 @@
+const gsheets = require('@googleapis/sheets');
+const { getCredentials } = require('./googleCredentials');
+
+const getSheets = async () => {
+  const auth = new gsheets.auth.GoogleAuth({
+    credentials: getCredentials(),
+    scopes: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/spreadsheets',
+    ],
+  });
+  const authClient = await auth.getClient();
+
+  return await gsheets.sheets({
+    auth: authClient,
+    version: "v4",
+  });
+};
+
+module.exports.appendToSpreadsheet = async (spreadsheetId, range, values) => {
+  const sheets = await getSheets();
+
+  const resource = {
+    values,
+  };
+
+  const res = await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range,
+    valueInputOption: 'RAW',
+    resource
+  });
+
+  return res.data;
+};
