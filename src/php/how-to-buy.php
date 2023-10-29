37cb163a0b86592e4fd7df9a624581dc355a1f17
Deyan Iolov <diolov@gmail.com>
2023-10-29 10:21:19 +0000

    add redirect to success page

--- src/php/how-to-buy.php
+++ src/php/how-to-buy.php
@@ -76,7 +76,15 @@
     <script type="text/javascript">
         window.addEventListener('DOMContentLoaded', event => {
             const howToBuyForm = document.getElementById('cta-how-to-buy');
-            howToBuyForm.onsubmit = async (event) => submitEmailFormAjax(event, howToBuyForm);
+            howToBuyForm.onsubmit = async (event) => {
+                submitEmailFormAjax(event, howToBuyForm)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(response => {
+                        window.location.assign("/success.html");
+                    });
+            }
         });
     </script>
 </body>
