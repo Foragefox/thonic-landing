28c000bc8646804fa3a228cac77adae55ba663db
Deyan Iolov <diolov@gmail.com>
2023-11-02 19:42:19 +0000

    Add how to buy link in header

--- src/php/parts/header.php
+++ src/php/parts/header.php
@@ -1,13 +1,13 @@
 <header class="navbar navbar-expand-lg">
     <div class="container">
         <a class="navbar-brand" href="/">
-          <img src="/img/logo-green.svg" />
-          <span class="title">THONIC</span>
+            <img src="/img/logo-green.svg" />
+            <span class="title">THONIC</span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button> 
-       
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <div class="col-lg">
                 <div class="menu justify-content-center">
@@ -19,6 +19,7 @@
             </div>
 
             <div class="col-auto text-lg-end">
+                <a href="/how-to-buy" target="_blank" class="btn btn-sm btn-outline-light me-2">HOW TO BUY</a>
                 <a href="<?= $pancakeSwap ?>" target="_blank" class="btn btn-sm btn-outline-light">BUY THONIC</a>
             </div>
         </div>
