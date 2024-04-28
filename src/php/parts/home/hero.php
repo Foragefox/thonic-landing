5d61a19f6ba1a5ee2815f31e9fcb0d78bb3f118b
Deyan Iolov <diolov@gmail.com>
2024-04-28 21:19:32 +0100

    Add "Sign up for free" button on the home page

--- src/php/parts/home/hero.php
+++ src/php/parts/home/hero.php
@@ -2,11 +2,11 @@
     <img class="hero-image" src="/img/screenshot.png" />
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-10 col-lg-9 col-xl-8 col-xxl-6">
+            <div class="col-10 col-lg-9 col-xl-8 col-xxl-7">
                 <div class="hero-body text-center">
                     <h1 class="display-3 mb-4">We create confident crypto investors</h1>
                     <p class="mb-4">Thonic is a platform that offers institutional investor-grade financial tools to help everyone invest in altcoins and manage crypto portfolios.</p>
-                  <!--  <a class="btn btn-green">Get Thonic to join</a> -->
+                    <p><a class="btn btn-green" href="https://app.thonic.finance/register">Sign up for free</a></p>
                 </div>
             </div>
         </div>
